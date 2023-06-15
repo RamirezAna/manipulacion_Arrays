de398dbf4c2d5a82ae906c80786ff372b75b9ed2
Ana Ramirez <aramirez.cac@gmail.com>
2023-06-15 14:47:13 -0400

    map

---
+++ server/02-map.js
@@ -0,0 +1,18 @@
+
+const letters = ['a', 'b', 'c'];
+
+
+//con for **************----------------
+
+//const newArray = [];
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
+
+//con map ***********--------------------
+const newArray = letters.map(item => item + '++')
+
+console.log('original ', letters);
+console.log('new ', newArray);
+
