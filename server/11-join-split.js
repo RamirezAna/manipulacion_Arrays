111b2608bb2af2d43c26f80dbba9fa2843d41f49
Ana Ramirez <aramirez.cac@gmail.com>
2023-06-19 10:21:53 -0400

    desafio

--- server/11-join-split.js
+++ server/11-join-split.js
@@ -21,3 +21,25 @@ const titleFinal =title.split(' ').join('-').toLowerCase();
 
 console.log(titleFinal);
 
+
+
+
+
+// En este desafío vas a recibir el título de un artículo y tú debes transformarlo en un formato de URL 
+// en donde normalmente se transforma todo en minúscula y se cambian los espacios por un guion (-).
+// Para solucionarlo vas a encontrar una función llamada parseToURL que recibe un parámetro de entrada:
+
+// title: Un String con el título.
+// Dentro del cuerpo de la función parseToURL debes escribir tu solución.
+
+// Ejemplo 1:
+
+// Input: "La forma de correr Python"
+// Output: "la-forma-de-correr-python"
+ 
+
+// export function parseToURL(title) {
+//     // Tu código aquí 👈
+//     return title.toLowerCase().replace(/ /g, "-");
+//   }
+    
